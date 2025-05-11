0408657aa180c2a52d069c0c832a570be8061318
Peterson <poseidoncenghizerzhemetmhossad@outlook.com>
2025-05-11 13:27:37 +0000

    SuperAct

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,25 @@
 import './App.scss';
 import { Todo, TodoList, User } from './components/TodoList';
 import todosFromServer from './api/todos';
-
 import usersFromServer from './api/users';
 import { Form } from './components/Form';
 import { useState } from 'react';
 
 export const App = () => {
   const [usersList] = useState<User[]>(usersFromServer);
-  const [todoList, setuTodoList] = useState<Todo[]>(todosFromServer);
+  const [todoList, setTodoList] = useState<Todo[]>(todosFromServer); // Fixed typo in setter name
 
   const addNewTodo = (newTodo: Todo) => {
-    setuTodoList((currentTodo: Todo[]) => [...currentTodo, newTodo]);
+    setTodoList(currentTodos => [...currentTodos, newTodo]); // Fixed setter name and simplified type
   };
 
   return (
-    <>
-      <div className="App">
-        <h1>Add todo form</h1>
+    <div className="App">
+      <h1>Add todo form</h1>
 
-        <Form users={usersList} todos={todoList} addFunc={addNewTodo} />
+      <Form users={usersList} todos={todoList} addFunc={addNewTodo} />
 
-        <TodoList todos={todoList} users={usersList} />
-      </div>
-    </>
+      <TodoList todos={todoList} users={usersList} />
+    </div>
   );
 };
